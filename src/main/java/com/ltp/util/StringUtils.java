f102d780f4e3409face5c3c68e65bec10dce309f
ALazyGuy <razorprogrammer@gmail.com>
2021-09-21 19:51:30 +0300

    feat: Implemented double numbers

--- src/main/java/com/ltp/util/StringUtils.java
+++ src/main/java/com/ltp/util/StringUtils.java
@@ -1,15 +1,26 @@
 package com.ltp.util;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class StringUtils {
 
-    public static boolean isPositiveNumber(String number){
-        if(Objects.isNull(number)){
+    public static boolean isPositiveNumber(String number) throws NumberFormatException{
+        if(Objects.isNull(number) || number.charAt(0) == '-'){
             return false;
         }
 
-        return number.startsWith("-") && org.apache.commons.lang3.StringUtils.isNumeric(number.substring(1));
+        if(number.charAt(0) == '+'){
+           number = number.substring(1);
+        }
+
+        String[] parts = number.split("\\.");
+
+        if(parts.length > 2 || Arrays.stream(parts).anyMatch(part -> !org.apache.commons.lang3.StringUtils.isNumeric(part))){
+            throw new NumberFormatException(number);
+        }
+
+        return true;
     }
 
 }
