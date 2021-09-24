bbdf1fa3ca4b4e071993da3fb415bf6f9c66d487
ALazyGuy <razorprogrammer@gmail.com>
2021-09-24 23:14:37 +0300

    feat: Implemented DataSource bean

---
+++ src/main/java/com/epam/esm/configuration/AppConfig.java
@@ -0,0 +1,24 @@
+package com.epam.esm.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.epam.esm")
+public class AppConfig {
+
+    @Bean
+    public DataSource getDataSource(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/second?serverTimezone=UTC");
+        driverManagerDataSource.setUsername("root");
+        driverManagerDataSource.setPassword("cocksucker");
+        return driverManagerDataSource;
+    }
+
+}
