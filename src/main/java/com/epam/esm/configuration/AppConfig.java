9dc5d37b13dac7227a88a2d1504a49a7751a0fbe
ALazyGuy <razorprogrammer@gmail.com>
2021-09-26 21:38:22 +0300

    fix: Configuration

--- src/main/java/com/epam/esm/configuration/AppConfig.java
+++ src/main/java/com/epam/esm/configuration/AppConfig.java
@@ -1,24 +1,53 @@
 package com.epam.esm.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.sql.DataSource;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan("com.epam.esm")
-public class AppConfig {
+@PropertySource("classpath:application.properties")
+public class AppConfig implements WebMvcConfigurer {
+
+    private final Environment env;
+
+    @Autowired
+    public AppConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public ViewResolver getViewResolver(){
+        return new InternalResourceViewResolver();
+    }
 
     @Bean
     public DataSource getDataSource(){
+        System.out.println(env.getProperty("jdbc.driver"));
+
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/second?serverTimezone=UTC");
-        driverManagerDataSource.setUsername("root");
-        driverManagerDataSource.setPassword("cocksucker");
+        driverManagerDataSource.setDriverClassName(env.getProperty("jdbc.driver"));
+        driverManagerDataSource.setUrl(env.getProperty("jdbc.url"));
+        driverManagerDataSource.setUsername(env.getProperty("jdbc.username"));
+        driverManagerDataSource.setPassword(env.getProperty("jdbc.password"));
         return driverManagerDataSource;
     }
 
+    @Bean
+    public JdbcTemplate getJdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
 }
