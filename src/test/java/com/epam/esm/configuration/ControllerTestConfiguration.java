0d9e88339cd61322d20c5eb1fb76e2e5ce5376e1
ALazyGuy <razorprogrammer@gmail.com>
2021-10-12 04:49:30 +0300

    feat: Implemented CertificateController tests

--- src/test/java/com/epam/esm/configuration/ControllerTestConfiguration.java
+++ src/test/java/com/epam/esm/configuration/ControllerTestConfiguration.java
@@ -1,6 +1,8 @@
 package com.epam.esm.configuration;
 
+import com.epam.esm.controller.CertificateController;
 import com.epam.esm.controller.TagController;
+import com.epam.esm.service.CertificateService;
 import com.epam.esm.service.TagService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,16 @@ public class ControllerTestConfiguration {
     @Bean
     public TagController tagController(){
         return new TagController(tagService());
+    }
+
+    @Bean
+    public CertificateService certificateService(){
+        return mock(CertificateService.class);
+    }
+
+    @Bean
+    public CertificateController certificateController(){
+        return new CertificateController(certificateService());
     }
 
     @Bean
